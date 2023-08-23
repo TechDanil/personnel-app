05114cdfc6c52eefe5af500db1d281c38d42c372
TechDanil <danilkomarov243242@gmail.com>
2023-08-23 17:27:48 +0500

    added Provider in main.tsx and gave it a store to affect on the app

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import AuthSlice from "../slices/Auth.slice";
 
 const store = configureStore({
     reducer: {
-
+        auth: AuthSlice,
     }
 });
 
